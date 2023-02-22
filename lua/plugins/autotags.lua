fd01b1db9e396883299b29e106d6552949b5aa00
Cong Nguyen <kcong.nguyen@icloud.com>
2023-02-21 23:55:50 -0600

    i

--- lua/plugins/autotags.lua
+++ lua/plugins/autotags.lua
@@ -1,3 +1,8 @@
+local treesitter_ok = pcall(require, "nvim-treesitter.configs")
+if not treesitter_ok then
+  return
+end
+
 local at_ok, at = pcall(require, "nvim-ts-autotag")
 if not at_ok then
   return
